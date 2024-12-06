7e81b25f075adcc65be8aff7414f6443564beba6
tt239 <tuanphamtran99@gmail.com>
2024-12-06 10:49:43 +0700

    Set up project

--- deploy/scripts/deploy_to_staging.sh
+++ deploy/scripts/deploy_to_staging.sh
@@ -3,6 +3,7 @@ CI_DEPLOY_USER="${CI_DEPLOY_USER:-root}"
 CI_DEPLOY_SSH_PORT="${CI_DEPLOY_SSH_PORT:-22}"
 CI_DEPLOY_HOST="$CI_DEPLOY_USER@$CI_DEPLOY_SERVER"
 
+echo "-------------------------- Script File ---------------------------------"
 
 # Copy necessary files to /app folder.
 scp -o StrictHostKeyChecking=no -r -P $CI_DEPLOY_SSH_PORT \
@@ -11,7 +12,7 @@ scp -o StrictHostKeyChecking=no -r -P $CI_DEPLOY_SSH_PORT \
   ./deploy/scripts/setup_staging_app.sh \
   $CI_DEPLOY_HOST:/usr/app/admin-server
 
-ssh -o StrictHostKeyChecking=no -p $CI_DEPLOY_SSH_PORT $CI_DEPLOY_HOST << 'ENDSSH' 
+ssh -o StrictHostKeyChecking=no -p $CI_DEPLOY_SSH_PORT $CI_DEPLOY_HOST << 'EOF'
   set -e
 
   cd /usr/app/admin-server
@@ -25,8 +26,8 @@ ssh -o StrictHostKeyChecking=no -p $CI_DEPLOY_SSH_PORT $CI_DEPLOY_HOST << 'ENDSS
   docker rmi travis99/admin-server
   
   # Set up application.
-  ./setup_staging_app.sh
+  bash ./setup_staging_app.sh
 
   # Clean up unused resources.
   sudo docker system prune --all --force &
-ENDSSH
+EOF
