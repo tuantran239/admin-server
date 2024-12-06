409bd3d4c91b749fc5f81a1bf105378b19204516
tt239 <tuanphamtran99@gmail.com>
2024-12-06 11:47:33 +0700

    Set up project

--- deploy/scripts/deploy_to_staging.sh
+++ deploy/scripts/deploy_to_staging.sh
@@ -29,6 +29,13 @@ ssh -o StrictHostKeyChecking=no -p $CI_DEPLOY_SSH_PORT $CI_DEPLOY_HOST << 'EOF'
 
   echo "---------Remove Image--------"
 
+  if docker images -q "$CI_IMAGE_NAME"  > /dev/null; then
+    docker rmi "$CI_IMAGE_NAME"
+    echo "Image $CI_IMAGE_NAME deleted."
+  else
+    echo "Image $CI_IMAGE_NAME does not exist."
+  fi
+
   docker rmi travis99/admin-server
   
   echo "---------Set up application.--------"
--- deploy/scripts/setup_deploy_env.sh
+++ deploy/scripts/setup_deploy_env.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+touch .env
+
 echo "# Starting additional CI variables" >> .env
 
 echo CI_REGISTRY_PASSWORD=$CI_REGISTRY_PASSWORD >> .env
 echo CI_REGISTRY_USER=$CI_REGISTRY_USER >> .env 
 echo CI_REGISTRY=$CI_REGISTRY >> .env
+echo CI_IMAGE_NAME=$CI_IMAGE_NAME >> .env
 
 echo $CI_DEPLOY_ENV_VARS | base64 -d >> .env
